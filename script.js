85a2b8db4fc1b9635614fac6868461bda8abed5e
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-03-12 23:08:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function shortcut(s1, s2) {
   // your code here
+	 if (!str1 || !str2) return "";
+  
+  // Return the first character of each string
+  return str1[0] + str2[0];
 }
 
-// Do not change the code below.
+// // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
 alert(shortcut(s1, s2));
