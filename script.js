1c26d1ae372144727cd9bd7b3b626852095c5f29
soumya773 <150830999+soumya773@users.noreply.github.com>
2025-04-08 11:15:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function shortcut(s1, s2) {
 	 if (!str1 || !str2) return "";
   
   // Return the first character of each string
-  return str1[0] + str2[0];
+  return str1[0] + str2[0] ;
 }
 
 // // Do not change the code below.
